0abc005fb2b15011a0bfb354e67d6454676ee84b
Sebastian Tramp <mail@sebastian.tramp.name>
2010-05-08 00:50:10 +0200

    add: desc and list methods add: all uri methods allow qnames now mod: re-structure

--- rdf.sh
+++ rdf.sh
@@ -1,25 +1,165 @@
 #!/bin/bash
 # @(#) A multi-tool shell script for doing Semantic Web jobs on the command line.
 
+this=`basename $0`
 command="$1"
 
 if [ "$command" == "" ]
 then
-    echo "Syntax:" `basename $0` "<command>"
-    echo "(command is one of: get head rdfhead ns diff count validate)"
+    echo "Syntax:" $this "<command>"
+    echo "(command is one of: get head rdfhead ns diff count desc list)"
     exit 1
 fi
 
+# takes an input string and checks if it is a valid qname
+_isQName ()
+{
+    qname=$1
+    if [ "$qname" == "" ]
+    then
+        echo "isQName error: need an parameter"
+        exit 1
+    fi
+
+    LocalPart=`echo $qname | cut -d ":" -f 2`
+    if [ "$qname" == "$LocalPart" ]
+    then
+        echo "false"
+        return
+    else
+        Prefix=`echo $qname | cut -d ":" -f 1`
+
+        # this is ugly ... here we distinguish between uris and qnames
+        case "$Prefix" in
+            "http" | "https" )
+                echo "false"
+                return
+            ;;
+        esac
+
+        if [ "$qname" != "$Prefix:$LocalPart" ]
+        then
+        echo "false"
+        return
+        else
+            echo "true"
+            return
+        fi
+    fi
+}
+
+# takes a qname and outputs the prefix
+_getPrefix ()
+{
+    qname=$1
+    if [ "$qname" == "" ]
+    then
+        echo "getPrefix error: need an qname parameter"
+        exit 1
+    fi
+
+    LocalPart=`echo $qname | cut -d ":" -f 2`
+    if [ "$qname" == "$LocalPart" ]
+    then
+        echo "getPrefix error: $qname is not a valid qname"
+        exit 1
+    else
+        Prefix=`echo $qname | cut -d ":" -f 1`
+        if [ "$qname" != "$Prefix:$LocalPart" ]
+        then
+        echo "getPrefix error: $qname is not a valid qname"
+        exit 1
+        else
+            echo $Prefix
+        fi
+    fi
+}
+
+# takes a qname and outputs the LocalName
+_getLocalName ()
+{
+    qname=$1
+    if [ "$qname" == "" ]
+    then
+        echo "getLocalName error: need an qname parameter"
+        exit 1
+    fi
+
+    LocalPart=`echo $qname | cut -d ":" -f 2`
+    if [ "$qname" == "$LocalPart" ]
+    then
+        echo "getLocalName error: $qname is not a valid qname"
+        exit 1
+    else
+        Prefix=`echo $qname | cut -d ":" -f 1`
+        if [ "$qname" != "$Prefix:$LocalPart" ]
+        then
+        echo "getLocalName error: $qname is not a valid qname"
+        exit 1
+        else
+            echo $LocalPart
+        fi
+    fi
+}
+
+# takes an input qname or URI and outputs the expanded full URI (if it is a qname)
+_expandQName ()
+{
+    input=$1
+    isQName=`_isQName $input`
+    if [ "$isQName" == "true" ]
+    then
+        prefix=`_getPrefix $input`
+        localName=`_getLocalName $input`
+        namespace=`$this ns $prefix`
+        echo $namespace$localName
+    else
+        echo $input
+    fi
+}
+
+
 case "$command" in
+
+"info" | "desc")
+    uri="$2"
+    if [ "$uri" == "" ]
+    then
+        echo "Syntax:" $this "$command <URI | Prefix:LocalPart>"
+        echo "(outputs an N3 description of the given resource)"
+        exit 1
+    fi
+    uri=`_expandQName $uri`
+    tmpfile=`tempfile -p rdfsh`
+    $this get $uri >$tmpfile
+    roqet -q -e "CONSTRUCT {<$uri> ?p ?o} WHERE {<$uri> ?p ?o}" -D $tmpfile | cwm --n3=q
+    rm $tmpfile
+;;
+
+"list" )
+    uri="$2"
+    if [ "$uri" == "" ]
+    then
+        echo "Syntax:" $this "$command <URI | Prefix:LocalPart>"
+        echo "(list resources which start with the given URI)"
+        exit 1
+    fi
+    uri=`_expandQName $uri`
+    tmpfile=`tempfile -p rdfsh`
+    $this get $uri >$tmpfile
+    roqet -q -e "SELECT DISTINCT ?s WHERE {?s ?p ?o. FILTER isURI(?s) } " -D $tmpfile 2>/dev/null | cut -d "<" -f 2 | cut -d ">" -f 1 | grep $uri
+    rm $tmpfile
+;;
 
 "get" | "ld" )
     uri="$2"
     if [ "$uri" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <URI>"
+        echo "Syntax:" $this "$command <URI | Prefix:LocalPart>"
         echo "(wgets rdf in xml to stdout)"
         exit 1
     fi
+    uri=`_expandQName $uri`
     wget -q -O - --header="Accept: application/rdf+xml" $uri
 ;;
 
@@ -27,10 +167,11 @@ case "$command" in
     uri="$2"
     if [ "$uri" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <URI>"
+        echo "Syntax:" $this "$command <URI | Prefix:LocalPart>"
         echo "(curls only the http header)"
         exit 1
     fi
+    uri=`_expandQName $uri`
     curl -I -X HEAD $uri
 ;;
 
@@ -38,10 +179,11 @@ case "$command" in
     uri="$2"
     if [ "$uri" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <URI>"
+        echo "Syntax:" $this "$command <URI | Prefix:LocalPart>"
         echo "(curls only the http header but accepts only rdf)"
         exit 1
     fi
+    uri=`_expandQName $uri`
     curl -I -X HEAD -H "Accept: application/rdf+xml" $uri
 ;;
 
@@ -50,7 +192,7 @@ case "$command" in
     suffix="$3"
     if [ "$prefix" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <prefix> <suffix>"
+        echo "Syntax:" $this "$command <prefix> <suffix>"
         echo "(catch the namespace from prefix.cc)"
         echo " suffix can be n3, rdfa, sparql, ...)"
         exit 1
@@ -68,7 +210,7 @@ case "$command" in
 	source2="$3"
     if [ "$source2" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <rdf-file-1> <rdf-file-2>"
+        echo "Syntax:" $this "$command <rdf-file-1> <rdf-file-2>"
         echo "(diff of two RDF files)"
         exit 1
     fi
@@ -83,7 +225,7 @@ case "$command" in
     file="$2"
     if [ "$file" == "" ]
     then
-        echo "Syntax:" `basename $0` "$command <file>"
+        echo "Syntax:" $this "$command <file>"
         echo "(count triples using rapper)"
         exit 1
     fi
@@ -94,7 +236,7 @@ case "$command" in
 #    file="$2"
 #    if [ "$file" == "" ]
 #    then
-#        echo "Syntax:" `basename $0` "$command <file>"
+#        echo "Syntax:" $this "$command <file>"
 #        echo "(validates rdf/xml using http://www.w3.org/RDF/Validator/ARPServlet)"
 #        exit 1
 #    fi
