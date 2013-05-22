3bc3e2e692ef0e94a40fe8b5feb8c5fe6bdcffe8
Sebastian Tramp <mail@sebastian.tramp.name>
2013-05-22 17:17:16 +0200

    change recipe and doap file for 0.6

--- brew/rdf.sh.rb
+++ brew/rdf.sh.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class RdfSh < Formula
   homepage 'https://github.com/seebi/rdf.sh'
-  url 'https://github.com/seebi/rdf.sh/archive/v0.5.tar.gz'
-  version '0.5'
-  sha1 '457740f1896a686ba46edc96ffb6e4b6e88fd9c7'
+  url 'https://github.com/seebi/rdf.sh/archive/v0.6.tar.gz'
+  version '0.6'
+  sha1 'cf327f45e17d83cd18282ffa5a3932a6'
 
   depends_on 'raptor'
   depends_on 'rasqal'
   depends_on 'curl'
 
   def install
-    bin.install('rdf.sh' => 'rdf')
+    bin.install('rdf' => 'rdf')
     man1.install('rdf.1')
     # todo: how to install zsh autocompletion?
   end
