dd12a57f19fa6ac06cf748458d8c94061a5534c7
Sebastian Tramp <mail@sebastian.tramp.name>
2016-05-17 19:40:11 +0200

    fix brew style and audit issues

--- brew/rdf.sh.rb
+++ brew/rdf.sh.rb
@@ -1,24 +1,21 @@
-require 'formula'
-
 class RdfSh < Formula
-  desc 'A multi-tool shell script for doing Semantic Web jobs on the command line'
-  homepage 'https://github.com/seebi/rdf.sh'
-  url 'https://github.com/seebi/rdf.sh/archive/v0.7.0.tar.gz'
-  head 'https://github.com/seebi/rdf.sh.git'
-  version '0.7.0'
-  sha256 '3210042265082092540e698202f6aa1a7dadefff97924c23ea9e2da18a8fa94b'
+  desc "multi-tool shell script for doing Semantic Web jobs on the command-line"
+  homepage "https://github.com/seebi/rdf.sh"
+  url "https://github.com/seebi/rdf.sh/archive/v0.7.0.tar.gz"
+  sha256 "3210042265082092540e698202f6aa1a7dadefff97924c23ea9e2da18a8fa94b"
+  head "https://github.com/seebi/rdf.sh.git"
 
-  depends_on 'raptor'
-  depends_on 'rasqal'
-  depends_on 'curl'
+  depends_on "raptor"
+  depends_on "rasqal"
+  depends_on "curl"
 
   def install
-    bin.install('rdf' => 'rdf')
-    man1.install('rdf.1')
-    zsh_completion.install '_rdf'
+    bin.install("rdf" => "rdf")
+    man1.install("rdf.1")
+    zsh_completion.install "_rdf"
   end
 
-  def test
+  test do
     system "rdf"
   end
 end
