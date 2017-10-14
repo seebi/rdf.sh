32cd7f3f137976375c10a67283a1fe30456beb5d
Sebastian Tramp <sebastian.tramp@eccenca.com>
2017-10-14 09:51:09 +0200

    fix broken testDesc

--- tests/desc_test.sh
+++ tests/desc_test.sh
@@ -2,6 +2,14 @@
 
 testDesc() {
     export RDFSH_HIGHLIGHTING_SUPPRESS=true
+    ../rdf ns rdfs
+    ../rdf ns rdf
+    ../rdf ns owl
+    ../rdf ns foaf
+    ../rdf ns contact
+    ../rdf ns geo
+    ../rdf ns schema
+    ../rdf ns vs
     ../rdf desc foaf:Person > test.ttl
     diff test.ttl foafPerson.ttl || fail "test.ttl and foafPerson.ttl are not same described."
     rm test.ttl
