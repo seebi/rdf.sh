435c63b6ffec0f12b1fc12630a12da626eb78175
Sebastian Tramp <sebastian.tramp@eccenca.com>
2017-10-14 16:23:08 +0200

    set version ids

--- brew/rdf.sh.rb
+++ brew/rdf.sh.rb
@@ -1,7 +1,7 @@
 class RdfSh < Formula
   desc "multi-tool shell script for doing Semantic Web jobs on the command-line"
   homepage "https://github.com/seebi/rdf.sh"
-  url "https://github.com/seebi/rdf.sh/archive/v0.7.0.tar.gz"
+  url "https://github.com/seebi/rdf.sh/archive/v0.8.0.tar.gz"
   sha256 "3210042265082092540e698202f6aa1a7dadefff97924c23ea9e2da18a8fa94b"
   head "https://github.com/seebi/rdf.sh.git"
 
